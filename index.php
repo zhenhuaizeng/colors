8865c1e2e0060adaa8ddacec8df4b420b59b67a8
Jack Zeng <zeng.zhenhuai@student.greenriver.edu>
2023-01-05 16:45:21 -0500

    Create color form

--- index.php
+++ index.php
@@ -9,5 +9,11 @@
 </head>
 <body>
     <h1>Pick a Color</h1>
+    <form method = "post" action = "confirm.php">
+        <label><input type = "radio" value = "red" name = "color"> Red </label> <br>
+        <label><input type = "radio" value = "Blue" name = "color"> Blue </label> <br>
+        <label><input type = "radio" value = "Yellow" name = "color"> Yellow </label> <br>
+        <input type = "submit" value ="submit">
+    </form>
 </body>
 </html>
